33606791addd22e07c0ef4e624922bed3a6d1fc6
alexizsz <alexissalazar9@gmail.com>
2024-09-19 19:09:39 +0200

    initial setup, restarting project

---
+++ src/test/java/com/alexis/demo/webserviceproject_v2/WebServiceProjectV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexis.demo.webserviceproject_v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceProjectV2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
