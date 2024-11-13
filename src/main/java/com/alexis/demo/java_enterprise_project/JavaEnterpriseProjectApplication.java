a720f1db999f7fd3379f12febee3b3b477337e7c
alexizsz <alexissalazar9@gmail.com>
2024-11-13 09:22:54 +0100

    init commit

---
+++ src/main/java/com/alexis/demo/java_enterprise_project/JavaEnterpriseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alexis.demo.java_enterprise_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEnterpriseProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaEnterpriseProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexis/demo/java_enterprise_project/JavaEnterpriseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexis.demo.java_enterprise_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaEnterpriseProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
